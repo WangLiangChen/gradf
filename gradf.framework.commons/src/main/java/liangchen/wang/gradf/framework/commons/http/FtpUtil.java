b5040fd88c821a9a80fd6811cdb82ce3cc46eea4
LiangChen.Wang <sintrue@hotmail.com>
2020-11-06 09:39:38 +0800

    ftp 改为异步

--- gradf.framework.commons/src/main/java/liangchen/wang/gradf/framework/commons/http/FtpUtil.java
+++ gradf.framework.commons/src/main/java/liangchen/wang/gradf/framework/commons/http/FtpUtil.java
@@ -1,11 +1,17 @@
 package liangchen.wang.gradf.framework.commons.http;
 
+import liangchen.wang.gradf.framework.commons.exception.ErrorException;
 import liangchen.wang.gradf.framework.commons.utils.StringUtil;
+import okhttp3.internal.Util;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author LiangChen.Wang
@@ -13,6 +19,13 @@ import java.io.InputStream;
 public enum FtpUtil {
     //
     INSTANCE;
+    private ExecutorService executorService;
+
+    FtpUtil() {
+        // 核心线程数0,最大线程数Integer.MAX_VALUE,空闲线程超时时间60 SECONDS , 线程等待队列SynchronousQueue(容量为0的队列)
+        executorService = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>(), Util.threadFactory("Ftp Client", false));
+    }
 
     public void download(String url, NetResponse netResponse) {
         URIResolver uriResolver = new URIResolver();
@@ -21,45 +34,49 @@ public enum FtpUtil {
     }
 
     public void download(URIResolver uriResolver, NetResponse netResponse) {
-        FTPClient ftpClient = new FTPClient();
-        ftpClient.setConnectTimeout(60000);
-        try {
-            ftpClient.connect(uriResolver.getHost(), uriResolver.getPort());
-            ftpClient.login(uriResolver.getUsername(), uriResolver.getPassword());
-            ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
-            if (!FTPReply.isPositiveCompletion(ftpClient.getReplyCode())) {
-                ftpClient.disconnect();
-                return;
-            }
-            String path = uriResolver.getPath();
-            if (StringUtil.INSTANCE.isNotBlank(path)) {
-                ftpClient.changeWorkingDirectory(path);
-            }
-            ftpClient.enterLocalPassiveMode();
-        } catch (IOException e) {
-            netResponse.onFailure(e);
-            return;
-        }
-        InputStream inputStream = null;
-        try {
-            //谁用谁关闭
-            inputStream = ftpClient.retrieveFileStream(uriResolver.getFileName());
-        } catch (IOException e) {
-            netResponse.onFailure(e);
-        } finally {
+        executorService.submit(() -> {
+            FTPClient ftpClient = new FTPClient();
+            ftpClient.setConnectTimeout(60000);
             try {
-                ftpClient.logout();
-                if (ftpClient.isConnected()) {
+                ftpClient.connect(uriResolver.getHost(), uriResolver.getPort());
+                ftpClient.login(uriResolver.getUsername(), uriResolver.getPassword());
+                ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
+                int replyCode = ftpClient.getReplyCode();
+                if (!FTPReply.isPositiveCompletion(replyCode)) {
                     ftpClient.disconnect();
+                    netResponse.onFailure(new ErrorException("replyCode:{}", replyCode));
+                    return;
                 }
+                String path = uriResolver.getPath();
+                if (StringUtil.INSTANCE.isNotBlank(path)) {
+                    ftpClient.changeWorkingDirectory(path);
+                }
+                ftpClient.enterLocalPassiveMode();
             } catch (IOException e) {
                 netResponse.onFailure(e);
                 return;
             }
-        }
-        // 提出来是为了不处理回调中的异常
-        if (null != inputStream) {
-            netResponse.onResponse(inputStream);
-        }
+            InputStream inputStream = null;
+            try {
+                //谁用谁关闭
+                inputStream = ftpClient.retrieveFileStream(uriResolver.getFileName());
+            } catch (IOException e) {
+                netResponse.onFailure(e);
+            } finally {
+                try {
+                    ftpClient.logout();
+                    if (ftpClient.isConnected()) {
+                        ftpClient.disconnect();
+                    }
+                } catch (IOException e) {
+                    netResponse.onFailure(e);
+                    return;
+                }
+            }
+            // 提出来是为了不处理回调中的异常
+            if (null != inputStream) {
+                netResponse.onResponse(inputStream);
+            }
+        });
     }
 }
