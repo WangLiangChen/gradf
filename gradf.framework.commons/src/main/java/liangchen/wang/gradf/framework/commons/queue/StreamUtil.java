b50b37747d8df536d4f1f46435b2bba3839d5c9f
王良辰 <sintrue@hotmail.com>
2020-12-16 14:09:54 +0800

    Create StreamUtil.java

---
+++ gradf.framework.commons/src/main/java/liangchen/wang/gradf/framework/commons/queue/StreamUtil.java
@@ -0,0 +1,70 @@
+public enum StreamUtil{
+  INSTANCE;
+  public <T> Stream<T> takeWhile(Stream<T> stream, Predicate<? super T> predicate) {
+    Spliterator<T> spliterator = stream.spliterator();
+    return StreamSupport.stream(new Spliterator<T>() {
+        boolean stillGoing = false;
+
+        @Override
+        public boolean tryAdvance(Consumer<? super T> action) {
+            boolean hasNext = spliterator.tryAdvance(item -> {
+                if (!predicate.test(item)) {
+                    action.accept(item);
+                    stillGoing = true;
+                } else {
+                    stillGoing = false;
+                }
+            });
+            return hasNext && stillGoing;
+        }
+
+        @Override
+        public Spliterator<T> trySplit() {
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return 0;
+        }
+
+        @Override
+        public int characteristics() {
+            return 0;
+        }
+    }, false);
+}
+public <T> Stream<T> dropWhile(Stream<T> stream, Predicate<? super T> predicate) {
+    Spliterator<T> spliterator = stream.spliterator();
+    return StreamSupport.stream(new Spliterator<T>() {
+        boolean canGo = false;
+
+        @Override
+        public boolean tryAdvance(Consumer<? super T> action) {
+            return spliterator.tryAdvance(item -> {
+                if (canGo) {
+                    action.accept(item);
+                } else {
+                    canGo = predicate.test(item);
+                }
+            });
+
+        }
+
+        @Override
+        public Spliterator<T> trySplit() {
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return 0;
+        }
+
+        @Override
+        public int characteristics() {
+            return 0;
+        }
+    }, false);
+}
+}
