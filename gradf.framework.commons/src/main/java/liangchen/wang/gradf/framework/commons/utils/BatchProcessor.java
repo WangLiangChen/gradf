2b53a6c1ccf578571d7b44dfd9e57a3461752d12
LiangChen.Wang <sintrue@hotmail.com>
2020-12-26 18:11:26 +0800

    增加超时构造

--- gradf.framework.commons/src/main/java/liangchen/wang/gradf/framework/commons/utils/BatchProcessor.java
+++ gradf.framework.commons/src/main/java/liangchen/wang/gradf/framework/commons/utils/BatchProcessor.java
@@ -17,14 +17,23 @@ import java.util.function.Consumer;
 public class BatchProcessor<E> {
     private final BlockingQueue<E> blockingQueue;
     private final int batchSize;
+    private final long timeout;
+    private final TimeUnit timeUnit;
     private Consumer<List<E>> consumer;
     private Runnable finishRunable;
-    private List<E> buffer;
+    private List<E> bufferList;
+    private boolean finished = false;
 
     public BatchProcessor(int batchSize) {
+        this(batchSize, 5, TimeUnit.SECONDS);
+    }
+
+    public BatchProcessor(int batchSize, long timeout, TimeUnit timeUnit) {
         this.batchSize = batchSize;
         this.blockingQueue = new ArrayBlockingQueue<>(batchSize);
-        this.buffer = new ArrayList<>(batchSize);
+        this.bufferList = new ArrayList<>(batchSize);
+        this.timeout = timeout;
+        this.timeUnit = timeUnit;
     }
 
     /*public void drain(E e) {
@@ -48,7 +57,10 @@ public class BatchProcessor<E> {
         finishRunable.run();
     }*/
 
-    public void put(E e) {
+    public boolean put(E e) {
+        if (finished) {
+            return false;
+        }
         if (null == consumer) {
             throw new InfoException("Set Consumer by method onConsume");
         }
@@ -57,6 +69,7 @@ public class BatchProcessor<E> {
         } catch (InterruptedException ex) {
             throw new RuntimeException(ex);
         }
+        return true;
     }
 
     public void onConsume(Consumer<List<E>> consumer) {
@@ -71,13 +84,14 @@ public class BatchProcessor<E> {
     private void onDrain() {
         ThreadPoolUtil.INSTANCE.getExecutorService().execute(() -> {
             while (true) {
-                buffer = new ArrayList<>(batchSize);
+                bufferList = new ArrayList<>(batchSize);
                 try {
-                    Queues.drain(blockingQueue, buffer, batchSize, 2, TimeUnit.SECONDS);
+                    Queues.drain(blockingQueue, bufferList, batchSize, timeout, timeUnit);
                 } catch (InterruptedException e) {
                     throw new ErrorException(e);
                 }
-                if (buffer.size() == 0) {
+                if (bufferList.size() == 0) {
+                    finished = true;
                     if (null != finishRunable) {
                         finishRunable.run();
                     }
@@ -86,7 +100,7 @@ public class BatchProcessor<E> {
                 if (null == consumer) {
                     break;
                 }
-                consumer.accept(buffer);
+                consumer.accept(bufferList);
             }
         });
     }
