b40f156e566549c5a307eace534791b2273f1510
LiangChen.Wang <wangliangchen@jd.com>
2021-04-20 15:54:14 +0800

    重写AbstractCacheManager 待继承

--- gradf.framework.cache.cluster/src/main/java/liangchen/wang/gradf/framework/cache/cluster/override/RedisCacheManager.java
+++ gradf.framework.cache.cluster/src/main/java/liangchen/wang/gradf/framework/cache/cluster/override/RedisCacheManager.java
@@ -1,14 +1,17 @@
 package liangchen.wang.gradf.framework.cache.cluster.override;
 
 import liangchen.wang.gradf.framework.cache.override.CacheManager;
+import liangchen.wang.gradf.framework.commons.lock.LocalLockUtil;
+import liangchen.wang.gradf.framework.commons.lock.LockReader;
 import org.springframework.cache.Cache;
 import org.springframework.data.redis.cache.RedisCache;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.lang.Nullable;
 
 import java.time.Duration;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -16,9 +19,11 @@ import java.util.concurrent.ConcurrentMap;
  * @author LiangChen.Wang 2021/4/16
  */
 public class RedisCacheManager extends org.springframework.data.redis.cache.RedisCacheManager implements CacheManager {
+    private final String LOCK_KEY = "RedisCacheManager";
     private final RedisTemplate<Object, Object> redisTemplate;
     private final RedisCacheWriter cacheWriter;
     private RedisCacheConfiguration defaultCacheConfig;
+    private final boolean allowInFlightCacheCreation;
     private final ConcurrentMap<String, Cache> cacheMap = new ConcurrentHashMap<>(16);
 
     public RedisCacheManager(RedisTemplate<Object, Object> redisTemplate, RedisCacheWriter cacheWriter, RedisCacheConfiguration defaultCacheConfig) {
@@ -26,6 +31,7 @@ public class RedisCacheManager extends org.springframework.data.redis.cache.Redi
         this.redisTemplate = redisTemplate;
         this.cacheWriter = cacheWriter;
         this.defaultCacheConfig = defaultCacheConfig;
+        this.allowInFlightCacheCreation = true;
     }
 
     public RedisCacheManager(RedisTemplate<Object, Object> redisTemplate, RedisCacheWriter cacheWriter, RedisCacheConfiguration defaultCacheConfig, String... initialCacheNames) {
@@ -33,6 +39,7 @@ public class RedisCacheManager extends org.springframework.data.redis.cache.Redi
         this.redisTemplate = redisTemplate;
         this.cacheWriter = cacheWriter;
         this.defaultCacheConfig = defaultCacheConfig;
+        this.allowInFlightCacheCreation = true;
     }
 
     public RedisCacheManager(RedisTemplate<Object, Object> redisTemplate, RedisCacheWriter cacheWriter, RedisCacheConfiguration defaultCacheConfig, boolean allowInFlightCacheCreation, String... initialCacheNames) {
@@ -40,6 +47,7 @@ public class RedisCacheManager extends org.springframework.data.redis.cache.Redi
         this.redisTemplate = redisTemplate;
         this.cacheWriter = cacheWriter;
         this.defaultCacheConfig = defaultCacheConfig;
+        this.allowInFlightCacheCreation = allowInFlightCacheCreation;
     }
 
     public RedisCacheManager(RedisTemplate<Object, Object> redisTemplate, RedisCacheWriter cacheWriter, RedisCacheConfiguration defaultCacheConfig, Map<String, RedisCacheConfiguration> initialCacheConfigurations) {
@@ -47,6 +55,7 @@ public class RedisCacheManager extends org.springframework.data.redis.cache.Redi
         this.redisTemplate = redisTemplate;
         this.cacheWriter = cacheWriter;
         this.defaultCacheConfig = defaultCacheConfig;
+        this.allowInFlightCacheCreation = true;
     }
 
     public RedisCacheManager(RedisTemplate<Object, Object> redisTemplate, RedisCacheWriter cacheWriter, RedisCacheConfiguration defaultCacheConfig, Map<String, RedisCacheConfiguration> initialCacheConfigurations, boolean allowInFlightCacheCreation) {
@@ -54,18 +63,53 @@ public class RedisCacheManager extends org.springframework.data.redis.cache.Redi
         this.redisTemplate = redisTemplate;
         this.cacheWriter = cacheWriter;
         this.defaultCacheConfig = defaultCacheConfig;
+        this.allowInFlightCacheCreation = allowInFlightCacheCreation;
     }
 
     @Override
+    @Nullable
+    public Cache getCache(String cacheName) {
+        return getCache(cacheName, 0L);
+    }
+
+    @Override
+    @Nullable
     public Cache getCache(String cacheName, long ttl) {
+        String lockKey = String.format("%s::%s", LOCK_KEY, cacheName);
+        return LocalLockUtil.INSTANCE.readWriteInReadWriteLock(lockKey, () -> {
+            Cache cache = this.cacheMap.get(cacheName);
+            if (null == cache) {
+                return null;
+            }
+            return new LockReader.LockValueWrapper<>(cache);
+        }, () -> {
+            Cache missingCache = getMissingCache(cacheName, ttl);
+            if (null == missingCache) {
+                return null;
+            }
+            missingCache = decorateCache(missingCache);
+            this.cacheMap.put(cacheName, missingCache);
+            return missingCache;
+        });
+    }
+
+    protected RedisCache getMissingCache(String cacheName, long ttl) {
+        RedisCacheConfiguration cacheConfig = defaultCacheConfig;
         if (ttl > 0) {
-            this.defaultCacheConfig = this.defaultCacheConfig.entryTtl(Duration.ofMillis(ttl));
+            cacheConfig = defaultCacheConfig.entryTtl(Duration.ofMillis(ttl));
         }
-        return getCache(cacheName);
+        return allowInFlightCacheCreation ? createRedisCache(cacheName, cacheConfig) : null;
     }
 
     @Override
     protected RedisCache createRedisCache(String name, RedisCacheConfiguration cacheConfig) {
         return new liangchen.wang.gradf.framework.cache.cluster.redis.RedisCache(name, cacheWriter, cacheConfig, redisTemplate);
+    }
+
+    @Override
+    public Collection<String> getCacheNames() {
+        Set<String> cacheNames = new LinkedHashSet<>(super.getCacheNames());
+        cacheNames.addAll(this.cacheMap.keySet());
+        return Collections.unmodifiableSet(cacheNames);
     }
 }
---
+++ gradf.framework.cache/src/main/java/liangchen/wang/gradf/framework/cache/override/AbstractCacheManager.java
@@ -0,0 +1,95 @@
+package liangchen.wang.gradf.framework.cache.override;
+
+import liangchen.wang.gradf.framework.commons.lock.LocalLockUtil;
+import liangchen.wang.gradf.framework.commons.lock.LockReader;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @author LiangChen.Wang 2021/4/20
+ */
+public abstract class AbstractCacheManager implements CacheManager, InitializingBean {
+    private final String LOCK_KEY = "AbstractCacheManager";
+    private boolean transactionAware = false;
+    private final ConcurrentMap<String, Cache> cacheMap = new ConcurrentHashMap<>(16);
+
+    @Override
+    public void afterPropertiesSet() {
+        initializeCaches();
+    }
+
+    public void initializeCaches() {
+        Collection<? extends Cache> caches = loadCaches();
+        synchronized (this.cacheMap) {
+            this.cacheMap.clear();
+            Set<String> cacheNames = new LinkedHashSet<>(caches.size());
+            for (Cache cache : caches) {
+                String name = cache.getName();
+                this.cacheMap.put(name, decorateCache(cache));
+                cacheNames.add(name);
+            }
+        }
+    }
+
+    public void setTransactionAware(boolean transactionAware) {
+        this.transactionAware = transactionAware;
+    }
+
+    public boolean isTransactionAware() {
+        return this.transactionAware;
+    }
+
+    protected abstract Collection<? extends Cache> loadCaches();
+
+    @Nullable
+    protected final Cache lookupCache(String name) {
+        return this.cacheMap.get(name);
+    }
+
+    @Override
+    public Collection<String> getCacheNames() {
+        return this.cacheMap.keySet();
+    }
+
+    @Override
+    @Nullable
+    public Cache getCache(String name) {
+        return getCache(name, 0L);
+    }
+
+    @Override
+    @Nullable
+    public Cache getCache(String name, long ttl) {
+        String lockKey = String.format("%s::%s", LOCK_KEY, name);
+        return LocalLockUtil.INSTANCE.readWriteInReadWriteLock(lockKey, () -> {
+            Cache cache = this.cacheMap.get(name);
+            if (null == cache) {
+                return null;
+            }
+            return new LockReader.LockValueWrapper<>(cache);
+        }, () -> {
+            Cache missingCache = getMissingCache(name, ttl);
+            if (null == missingCache) {
+                return null;
+            }
+            missingCache = decorateCache(missingCache);
+            this.cacheMap.put(name, missingCache);
+            return missingCache;
+        });
+    }
+
+    protected Cache decorateCache(Cache cache) {
+        return (isTransactionAware() ? new TransactionAwareCacheDecorator(cache) : cache);
+    }
+
+    @Nullable
+    protected Cache getMissingCache(String name, long ttl) {
+        return null;
+    }
+}
