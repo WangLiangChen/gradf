e08da3b635dd8284e91fcb70bf9436a309a37f9b
LiangChen.Wang <wangliangchen@jd.com>
2021-06-09 20:09:46 +0800

    SequenceNumber 测试完成

--- gradf.framework.data/src/main/java/liangchen/wang/gradf/framework/data/dao/impl/SequenceDaoImpl.java
+++ gradf.framework.data/src/main/java/liangchen/wang/gradf/framework/data/dao/impl/SequenceDaoImpl.java
@@ -1,13 +1,13 @@
 package liangchen.wang.gradf.framework.data.dao.impl;
 
+import liangchen.wang.gradf.framework.commons.exception.ErrorException;
+import liangchen.wang.gradf.framework.commons.exception.InfoException;
 import liangchen.wang.gradf.framework.data.condition.JdbcConditionAnnotation;
 import liangchen.wang.gradf.framework.data.dao.ISequenceDao;
-import liangchen.wang.gradf.framework.data.utils.TransactionUtil;
-import org.springframework.jdbc.core.JdbcTemplate;
+import liangchen.wang.gradf.framework.data.enumeration.DbConnectionsManager;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Isolation;
 
-import javax.inject.Inject;
+import java.sql.*;
 
 /**
  * @author LiangChen.Wang
@@ -15,28 +15,60 @@ import javax.inject.Inject;
 @JdbcConditionAnnotation
 @Repository("Gradf_Data_SequenceDao")
 public class SequenceDaoImpl implements ISequenceDao {
-    private final String SELECT_SQL = "select sequence_number from gradf_sequence where sequence_key=? for update";
+    private final String SELECT_SQL = "select sequence_number from gradf_sequence where sequence_key=?";
     private final String UPDATE_SQL = "update gradf_sequence set sequence_number=sequence_number+1 where sequence_key=?";
     private final String INSERT_SQL = "insert into gradf_sequence values(?,?)";
-    private final JdbcTemplate jdbcTemplate;
-
-    @Inject
-    public SequenceDaoImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
 
     @Override
     public Long sequenceNumber(String sequenceKey, long initialValue) {
-        return TransactionUtil.INSTANCE.execute(() -> {
-            Long sequenceNumber = jdbcTemplate.queryForObject(SELECT_SQL, Long.class, new Object[]{sequenceKey});
-            if (null == sequenceNumber) {
-                jdbcTemplate.update(INSERT_SQL, new Object[]{sequenceKey, initialValue});
-                return sequenceNumber;
+        for (int i = 0; i < 3; i++) {
+            long sequenceNumber = fetchSequenceNumber(sequenceKey, initialValue);
+            if (-1 == sequenceNumber) {
+                continue;
             }
-            jdbcTemplate.update(UPDATE_SQL, new Object[]{sequenceKey});
             return sequenceNumber;
-        }, Isolation.REPEATABLE_READ);
+        }
+        throw new InfoException("Could't fetch sequence number!");
     }
+
+    private long fetchSequenceNumber(String sequenceKey, long initialValue) {
+        return DbConnectionsManager.INSTANCE.executeInNonManagedConnection((connection) -> {
+            try {
+                // 先更新
+                PreparedStatement statement = connection.prepareStatement(UPDATE_SQL);
+                statement.setString(1, sequenceKey);
+                int rows = statement.executeUpdate();
+                statement.close();
+                // 更新成功,说明数据存在，则查询返回
+                if (1 == rows) {
+                    statement = connection.prepareStatement(SELECT_SQL);
+                    statement.setString(1, sequenceKey);
+                    ResultSet resultSet = statement.executeQuery();
+                    Long sequenceNumber = null;
+                    if (resultSet.next()) {
+                        sequenceNumber = resultSet.getLong("sequence_number");
+                    }
+                    resultSet.close();
+                    statement.close();
+                    return sequenceNumber;
+                }
+                // 更新失败则尝试插入
+                try {
+                    statement = connection.prepareStatement(INSERT_SQL);
+                    statement.setString(1, sequenceKey);
+                    statement.setLong(2, initialValue);
+                    statement.executeUpdate();
+                    statement.close();
+                    return initialValue;
+                } catch (SQLIntegrityConstraintViolationException ex) {
+                    // 主键冲突 插入失败
+                    return -1L;
+                }
+            } catch (SQLException e) {
+                throw new ErrorException(e);
+            }
+        }, Connection.TRANSACTION_READ_COMMITTED);
+    }
+
 
 }
--- gradf.framework.data/src/test/java/liangchen/wang/gradf/framework/data/test/DataTest.java
+++ gradf.framework.data/src/test/java/liangchen/wang/gradf/framework/data/test/DataTest.java
@@ -1,10 +1,12 @@
 package liangchen.wang.gradf.framework.data.test;
 
 import liangchen.wang.gradf.framework.data.annotation.EnableJdbc;
+import liangchen.wang.gradf.framework.data.manager.ISequenceManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.inject.Inject;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author LiangChen.Wang 2020/9/16
@@ -13,11 +15,35 @@ import javax.inject.Inject;
 @EnableJdbc
 public class DataTest {
     @Inject
-    private ISwitchService switchService;
+    private ISequenceManager sequenceManager;
 
     @Test
-    public void testTransactional(){
-        switchService.testTransactional();
+    public void testSecquenceNumber() throws InterruptedException {
+        int count = 40;
+        CountDownLatch countDownLatch = new CountDownLatch(count);
+        for (int i = 0; i < count / 4; i++) {
+            new Thread(() -> {
+                Long number = sequenceManager.sequenceNumber("w1");
+                System.out.println("w1==========>" + number);
+                countDownLatch.countDown();
+            }).start();
+            new Thread(() -> {
+                Long number = sequenceManager.sequenceNumber("w3");
+                System.out.println("w3==========>" + number);
+                countDownLatch.countDown();
+            }).start();
+            new Thread(() -> {
+                Long number = sequenceManager.sequenceNumber("w5");
+                System.out.println("w5==========>" + number);
+                countDownLatch.countDown();
+            }).start();
+            new Thread(() -> {
+                Long number = sequenceManager.sequenceNumber("w4");
+                System.out.println("w4==========>" + number);
+                countDownLatch.countDown();
+            }).start();
+        }
+        countDownLatch.await();
     }
 
 }
