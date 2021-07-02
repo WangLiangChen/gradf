f6f999bc53e13738d3971edd0e8c0263dc799654
LiangChen.Wang <wangliangchen@jd.com>
2021-07-02 20:24:59 +0800

    StartProcessMonitor

--- gradf.framework.springboot/src/main/java/liangchen/wang/gradf/framework/springboot/monitor/StartProcessMonitor.java
+++ gradf.framework.springboot/src/main/java/liangchen/wang/gradf/framework/springboot/monitor/StartProcessMonitor.java
@@ -1,22 +1,45 @@
 package liangchen.wang.gradf.framework.springboot.monitor;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.*;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.context.*;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringValueResolver;
 
 /**
  * @author LiangChen.Wang 2021/7/2
  */
 @Component
-public class StartProcessMonitor implements EnvironmentPostProcessor
-        , ApplicationContextInitializer<ConfigurableApplicationContext>
-        , SpringApplicationRunListener
-        , ApplicationListener<ApplicationEvent>
-        , Lifecycle
-        , ApplicationRunner
-        , CommandLineRunner {
+public class StartProcessMonitor implements EnvironmentPostProcessor,
+        ApplicationContextInitializer<ConfigurableApplicationContext>,
+        SpringApplicationRunListener,
+        ApplicationListener<ApplicationEvent>,
+        Lifecycle,
+        ApplicationRunner,
+        CommandLineRunner,
+        BeanDefinitionRegistryPostProcessor,
+        InstantiationAwareBeanPostProcessor,
+        InitializingBean,
+        DisposableBean,
+        BeanNameAware,
+        BeanFactoryAware,
+        ApplicationContextAware,
+        BeanClassLoaderAware,
+        EnvironmentAware,
+        ResourceLoaderAware,
+        EmbeddedValueResolverAware,
+        ApplicationEventPublisherAware,
+        MessageSourceAware {
     private final String prefix = "------ StartProcessMonitor,interface:%s,method:%s";
     private boolean isRunning;
 
@@ -102,5 +125,100 @@ public class StartProcessMonitor implements EnvironmentPostProcessor
     @Override
     public boolean isRunning() {
         return this.isRunning;
+    }
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
+        System.out.println(String.format(prefix, "BeanDefinitionRegistryPostProcessor", "postProcessBeanDefinitionRegistry"));
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        System.out.println(String.format(prefix, "BeanFactoryPostProcessor", "postProcessBeanFactory"));
+    }
+
+    @Override
+    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+        System.out.println(String.format(prefix, "InstantiationAwareBeanPostProcessor", "postProcessBeforeInstantiation"));
+        return null;
+    }
+
+    @Override
+    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+        System.out.println(String.format(prefix, "InstantiationAwareBeanPostProcessor", "postProcessAfterInstantiation"));
+        return true;
+    }
+
+    @Override
+    public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+        System.out.println(String.format(prefix, "InstantiationAwareBeanPostProcessor", "postProcessProperties"));
+        return pvs;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(String.format(prefix, "BeanPostProcessor", "postProcessBeforeInitialization"));
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(String.format(prefix, "BeanPostProcessor", "postProcessAfterInitialization"));
+        return bean;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println(String.format(prefix, "DisposableBean", "destroy"));
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(String.format(prefix, "InitializingBean", "afterPropertiesSet"));
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println(String.format(prefix, "BeanClassLoaderAware", "setBeanClassLoader"));
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(String.format(prefix, "BeanFactoryAware", "setBeanFactory"));
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println(String.format(prefix, "BeanNameAware", "setBeanName"));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(String.format(prefix, "ApplicationContextAware", "setApplicationContext"));
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        System.out.println(String.format(prefix, "ApplicationEventPublisherAware", "setApplicationEventPublisher"));
+    }
+
+    @Override
+    public void setEmbeddedValueResolver(StringValueResolver resolver) {
+        System.out.println(String.format(prefix, "EmbeddedValueResolverAware", "setEmbeddedValueResolver"));
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        System.out.println(String.format(prefix, "EnvironmentAware", "setEnvironment"));
+    }
+
+    @Override
+    public void setMessageSource(MessageSource messageSource) {
+        System.out.println(String.format(prefix, "MessageSourceAware", "setMessageSource"));
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        System.out.println(String.format(prefix, "ResourceLoaderAware", "setResourceLoader"));
     }
 }
